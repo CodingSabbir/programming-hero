c2947171fb2589eae1a8f3ecbd7493769f154942
sabbir <mdsabbirrahmanm740@gmail.com>
2023-09-14 20:30:35 +0600

    event handler

--- src/componets/Event.jsx
+++ src/componets/Event.jsx
@@ -1,5 +1,31 @@
 import { useState } from "react";
 
+const MovieCounter=()=>{
+const [count,setCount]=useState(0)
+
+const changeValue=(action)=>{
+    if(action ==='increment'){
+        setCount(count + 1)
+    }else if(action ==='decrement' && count > 0){
+        setCount(count - 1)
+    }
+}
+    return(
+        <>
+            <div className="text-center">
+            <p className=" text-[25px]" >Number Of Movie:{count}</p>
+            <button onClick={()=>changeValue('increment')} className="text-[20px] text-center px-5">Add Movie</button>
+            <button onClick={()=>changeValue('decrement')} className="text-[20px] text-center">Remove Movie</button>
+            </div>
+        </>
+    )
+
+}
+
+
+
+
+
 
 
 
@@ -10,6 +36,7 @@ const Event = () => {
         name:'taskin',
         age:33,
     })
+
 
     const CuntEvent=()=>{
             setCount(
@@ -25,6 +52,7 @@ const Event = () => {
     }
     return(
         <>
+       <MovieCounter/>
             <h1 className="text-[25px] font-bold">Count:{count.name}</h1>
             <button onClick={CuntEvent}>Click me</button>
         </>
